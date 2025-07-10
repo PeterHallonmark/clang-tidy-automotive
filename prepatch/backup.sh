8923a8e26f34d06d56915bc2e40e5a0262c637c3
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-07-10 16:41:48 +0200

    Added a script to easier be able to commit.

---
+++ prepatch/backup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+# Copy the prepatched files to activate Automotive support 
+cp ../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h .
+cp ../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidy.cpp .
+cp ../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/CMakeLists.txt .
+cp ../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.h .
+cp ../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.cpp .
+cp ../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyModule.cpp .
