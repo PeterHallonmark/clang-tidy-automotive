a3ea6f3da312837f3c29f27ba994f6d2eece1c09
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-08-21 07:56:51 +0200

    Updated compiler and trying to get my new computer working. :)

--- configure.sh
+++ configure.sh
@@ -2,9 +2,11 @@
 mkdir build
 cd build
 cmake -G Ninja \
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON . \
+      -DCMAKE_CXX_COMPILER=/usr/bin/clang++-20 \
+      -DCMAKE_C_COMPILER=/usr/bin/clang-20 \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
       -DCMAKE_BUILD_TYPE=Release \
       -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" \
       -DLLVM_TARGETS_TO_BUILD="X86" \
-      ../llvm-project-llvmorg-20.1.7/llvm
+      ../llvm-project-llvmorg-20.1.8/llvm
 cd ..
--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 # Create symbolic links from the clang-tidy-automotive source tree into LLVM.
-ln -f -s ../../../src/automotive/ llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/
-ln -f -s ../../../../../test/checkers/automotive llvm-project-llvmorg-20.1.7/clang-tools-extra/test/clang-tidy/checkers/automotive
-ln -f -s ../../../src/ClangTidyDiagnosticMapping.cpp llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.cpp
-ln -f -s ../../../src/ClangTidyDiagnosticMapping.h llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.h
+ln -f -s ../../../src/automotive/ llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/
+ln -f -s ../../../../../test/checkers/automotive llvm-project-llvmorg-20.1.8/clang-tools-extra/test/clang-tidy/checkers/automotive
+ln -f -s ../../../src/ClangTidyDiagnosticMapping.cpp llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.cpp
+ln -f -s ../../../src/ClangTidyDiagnosticMapping.h llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.h
 
 # Create symlinks in the src directory.
 directories=(
@@ -27,16 +27,16 @@ src_files=(
 )
 
 for directory in "${directories[@]}"; do
-    ln -f -s "../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/${directory}" "src/${directory}"
+    ln -f -s "../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/${directory}" "src/${directory}"
 done
 
 for file in "${src_files[@]}"; do
-    ln -f -s "../llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/${file}" "src/${file}"
+    ln -f -s "../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/${file}" "src/${file}"
 done
 
 # Copy the prepatched files to activate Automotive support
-cp prepatch/ClangTidyForceLinker.h llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
-cp prepatch/ClangTidy.cpp llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidy.cpp
-cp prepatch/CMakeLists.txt llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/CMakeLists.txt
-cp prepatch/ClangTidyDiagnosticConsumer.h llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.h
-cp prepatch/ClangTidyDiagnosticConsumer.cpp llvm-project-llvmorg-20.1.7/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.cpp
+cp prepatch/ClangTidyForceLinker.h llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
+cp prepatch/ClangTidy.cpp llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidy.cpp
+cp prepatch/CMakeLists.txt llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/CMakeLists.txt
+cp prepatch/ClangTidyDiagnosticConsumer.h llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.h
+cp prepatch/ClangTidyDiagnosticConsumer.cpp llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.cpp
