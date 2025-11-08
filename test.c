346fa0cf43571240b215cc32833dd241f1d96d50
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-11-08 23:44:44 +0100

    Added all the test script and config file.

--- test.c
+++ test.c
@@ -1,8 +1,10 @@
 // A comment \
 still a comment
 
+int x;
 
 int main(void)
 {
+  int x;
   return 0;
 }
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-build/bin/clang-tidy test.c --
+build/bin/clang-tidy test.c -config-file=./.clang-tidy --
