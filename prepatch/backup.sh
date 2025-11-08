99a95b5183c6e73c985179152353a7df43ffa1e7
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-11-08 22:57:13 +0100

    Updated script to also include ClangTidyOption

--- prepatch/backup.sh
+++ prepatch/backup.sh
@@ -2,6 +2,8 @@
 # Copy the prepatched files back to the prepatch directory to easier be able to commit 
 cp ../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h .
 cp ../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidy.cpp .
+cp ../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyOptions.h .
+cp ../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyOptions.cpp .
 cp ../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/CMakeLists.txt .
 cp ../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.h .
 cp ../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.cpp .
