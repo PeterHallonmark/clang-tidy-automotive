6e431105c6409a62feca805e04e8e75a3bf8479c
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-12-21 22:24:11 +0100

    Corrected test case.

--- test/checkers/automotive/comment/avoid-line-splicing-within-comment.c
+++ test/checkers/automotive/comment/avoid-line-splicing-within-comment.c
@@ -1,25 +1,25 @@
-// RUN: %check_clang_tidy %s avoid-line-splicing-within-comment %t -- -- -std=c99
-// RUN: %check_clang_tidy %s avoid-line-splicing-within-comment %t -- -- -std=c11
+// RUN: %check_clang_tidy %s automotive-avoid-line-splicing-within-comment %t -- -- -std=c99
+// RUN: %check_clang_tidy %s automotive-avoid-line-splicing-within-comment %t -- -- -std=c11
 
 // A comment              Compliant
 
 // Another comment \
    with a line splice     Not compliant
-// CHECK-MESSAGES: :[[@LINE-2]]:20: warning: avoid line-spliceing within a '//' comment [avoid-line-splicing-within-comment]
+// CHECK-MESSAGES: :[[@LINE-2]]:20: warning: avoid line-spliceing within a '//' comment [automotive-avoid-line-splicing-within-comment]
 
 // Another comment \
    with two \
    line splices           Not compliant
-// CHECK-MESSAGES: :[[@LINE-3]]:20: warning: avoid line-spliceing within a '//' comment [avoid-line-splicing-within-comment]
-// CHECK-MESSAGES: :[[@LINE-3]]:13: warning: avoid line-spliceing within a '//' comment [avoid-line-splicing-within-comment]
+// CHECK-MESSAGES: :[[@LINE-3]]:20: warning: avoid line-spliceing within a '//' comment [automotive-avoid-line-splicing-within-comment]
+// CHECK-MESSAGES: :[[@LINE-3]]:13: warning: avoid line-spliceing within a '//' comment [automotive-avoid-line-splicing-within-comment]
 
 /\
 /\
 /\
 printf("chrismas tree");  Not compliant 
-// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [avoid-line-splicing-within-comment]
-// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [avoid-line-splicing-within-comment]
-// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [avoid-line-splicing-within-comment]
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [automotive-avoid-line-splicing-within-comment]
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [automotive-avoid-line-splicing-within-comment]
+// CHECK-MESSAGES: :[[@LINE-4]]:2: warning: avoid line-spliceing within a '//' comment [automotive-avoid-line-splicing-within-comment]
 
 /* \
                           Compliant - block comment '/*' allows line splice
