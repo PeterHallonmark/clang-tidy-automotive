fe4b0cba4c2c49cedb5db40441813b020006cdd5
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-12-22 02:20:01 +0100

    Fixed test for automotive-unused-label

--- test/checkers/automotive/statement/unused-label.c
+++ test/checkers/automotive/statement/unused-label.c
@@ -1,13 +1,27 @@
-// RUN: %check_clang_tidy %s misra-c2023-adv-2.6 %t -- -- -std=c90
-// RUN: %check_clang_tidy %s misra-c2023-adv-2.6 %t -- -- -std=c99
-// RUN: %check_clang_tidy %s misra-c2023-adv-2.6 %t -- -- -std=c11
-	
+// Test file for: automotive-unused-label
+//
+// This file tests that there is no unused label.
+
+// RUN: %check_clang_tidy %s automotive-unused-label %t -- -- -std=c90
+// RUN: %check_clang_tidy %s automotive-unused-label %t -- -- -std=c99
+// RUN: %check_clang_tidy %s automotive-unused-label %t -- -- -std=c11
+
+//===----------------------------------------------------------------------===//
+// Violation Cases (should trigger warnings)
+//===----------------------------------------------------------------------===//
+
 void f(void) {
 Label1:        /* Not compliant */
-// CHECK-MESSAGES: :[[@LINE-1]]:1: warning: unused label 'label1' [misra-c2023-adv-2.6]
+// CHECK-MESSAGES: :[[@LINE-1]]:1: warning: unused label 'Label1'
+  (void)0;  // Prevent C23 extension warning about label at end of compound statement
 }
+
+//===----------------------------------------------------------------------===//
+// Compliant Cases (should NOT trigger warnings)
+//===----------------------------------------------------------------------===//
 
 void g(void) {
   goto Label2;
 Label2:        /* Compliant */
+  (void)0;  // Prevent C23 extension warning
 }
