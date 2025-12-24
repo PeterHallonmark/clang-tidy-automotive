ecb835cc1df2ab9058f51997508aa24cf9f19ca8
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-12-24 20:06:01 +0100

    Corrected automotive-avoid-boolean-in-switch test case.

--- test/checkers/automotive/statement/no-boolean-in-switch.c
+++ test/checkers/automotive/statement/no-boolean-in-switch.c
@@ -1,6 +1,6 @@
-// RUN: %check_clang_tidy %s automotive-avoid-boolean-in-switch-stmt %t -- -- -std=c90
-// RUN: %check_clang_tidy %s automotive-avoid-boolean-in-switch-stmt %t -- -- -std=c99
-// RUN: %check_clang_tidy %s automotive-avoid-boolean-in-switch-stmt %t -- -- -std=c11
+// RUN: %check_clang_tidy %s automotive-avoid-boolean-in-switch %t -- -- -std=c90
+// RUN: %check_clang_tidy %s automotive-avoid-boolean-in-switch %t -- -- -std=c99
+// RUN: %check_clang_tidy %s automotive-avoid-boolean-in-switch %t -- -- -std=c11
 
 #include <stdbool.h>
 
@@ -9,7 +9,7 @@ void testSwitchWithBoolean() {
 
   // Test direct boolean variable
   switch (flag) {  // Not compliant
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement
     case true:
       break;
     case false:
@@ -18,7 +18,7 @@ void testSwitchWithBoolean() {
 
   // Test logical NOT operation
   switch (!flag) {  // Not compliant
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement
     case true:
       break;
     default:
@@ -28,7 +28,7 @@ void testSwitchWithBoolean() {
   // Test comparison operation
   int x = 5;
   switch (x == 0) {  // Not compliant
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement
     case true:
       break;
     case false:
@@ -38,7 +38,7 @@ void testSwitchWithBoolean() {
   // Test logical OR operation
   int y = 10;
   switch ((x == 5) || (y == 10)) {  // Not compliant
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement
     case true:
       break;
     case false:
@@ -47,7 +47,7 @@ void testSwitchWithBoolean() {
 
   // Test logical AND operation
   switch ((x > 0) && (y < 20)) {  // Not compliant
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement
     case true:
       break;
     case false:
@@ -56,7 +56,7 @@ void testSwitchWithBoolean() {
 
   // Test parenthesized boolean expression
   switch ((x != 10)) {  // Not compliant
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement
     case true:
       break;
     case false:
@@ -65,7 +65,7 @@ void testSwitchWithBoolean() {
 
   // Test ternary operator returning boolean
   switch ((x > 0) ? true : false) {  // Non-compliant
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
+  // : :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement
     case true:
       break;
     case false:
@@ -74,7 +74,6 @@ void testSwitchWithBoolean() {
 
   // Test explicitly cast boolean to int
   switch ((int)(x == 5)) {  // Compliant (allowed since cast forces integer type)
-  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: avoid boolean expression in switch statement [automotive-avoid-boolean-in-switch-stmt]
     case 0:
       break;
     case 1:
