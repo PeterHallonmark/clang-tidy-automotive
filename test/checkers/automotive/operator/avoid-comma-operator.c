607e8c76d7cf22af43f52b05a3e89a06d26e547c
Peter Hallonmark <peter.hallonmark@gmail.com>
2026-01-01 22:49:33 +0100

    Fixed missing -1.

--- test/checkers/automotive/operator/avoid-comma-operator.c
+++ test/checkers/automotive/operator/avoid-comma-operator.c
@@ -4,10 +4,10 @@
 
 void test() {
   int a = (1, 2);                               /* not complaint */
-  // CHECK-MESSAGES: [[@LINE]]:13: warning: avoid comma operator [automotive-avoid-comma-operator]
+  // CHECK-MESSAGES: [[@LINE-1]]:13: warning: avoid comma operator
   
   for (int i = 0, j = 1; i < 10; ++i, ++j) {    /* Not complaint */
-  // CHECK-MESSAGES: [[@LINE]]:37: warning: avoid comma operator [automotive-avoid-comma-operator]
+  // CHECK-MESSAGES: [[@LINE-1]]:37: warning: avoid comma operator
   }
 }
 
