0e3b049951856125e8309effabf0968e44a61efd
Peter Hallonmark <peter.hallonmark@gmail.com>
2026-01-02 23:00:17 +0100

    Updated test case to use the new template.

--- test/checkers/automotive/array/avoid-flexible-array-member.c
+++ test/checkers/automotive/array/avoid-flexible-array-member.c
@@ -1,12 +1,39 @@
+// Test file for: automotive-avoid-flexible-array-member
+//
+// This file tests detection of flexible array members in structure
+// definitions. Flexible array members (incomplete array types as the
+// last struct member) are prohibited and shall trigger diagnostics
+// when used, regardless of whether the structure contains preceding
+// members or consists solely of the flexible array.
+
 // RUN: %check_clang_tidy %s automotive-avoid-flexible-array-member %t -- -- -std=c11
 
 #include <stdint.h>
 
+//===----------------------------------------------------------------------===//
+// Violation Cases (should trigger warnings)
+//===----------------------------------------------------------------------===//
+
 struct s {
   uint32_t data[]; // Not compliant
-// CHECK-MESSAGES: :[[@LINE-1]]:12: warning: avoid flexible array members [automotive-avoid-flexible-array-member]
+// CHECK-MESSAGES: :[[@LINE-1]]:12: warning: avoid flexible array members
 };
 
+struct s2 {
+  uint32_t header;
+  uint32_t data[]; // Not compliant
+// CHECK-MESSAGES: :[[@LINE-1]]:12: warning: avoid flexible array members
+};
+
+//===----------------------------------------------------------------------===//
+// Compliant Cases (should NOT trigger warnings)
+//===----------------------------------------------------------------------===//
+
 struct t {
+  uint32_t data[3];
+};
+
+struct t2 {
+  uint32_t header;
   uint32_t data[3];
 };
