c71d0b68409713b03d683e6c5bc823022dd0089d
Peter Hallonmark <peter.hallonmark@gmail.com>
2026-01-17 22:17:10 +0100

    The in-tree is broken right now. Need to rework it.

--- backup/setup.sh
+++ backup/setup.sh
@@ -3,38 +3,10 @@
 # Create symbolic links from the clang-tidy-automotive source tree into LLVM.
 ln -f -s ../../../src/automotive/ llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/
 ln -f -s ../../../../../test/checkers/automotive llvm-project-llvmorg-20.1.8/clang-tools-extra/test/clang-tidy/checkers/automotive
-ln -f -s ../../../src/ClangTidyDiagnosticMapping.cpp llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.cpp
-ln -f -s ../../../src/ClangTidyDiagnosticMapping.h llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.h
-
-# Create symlinks in the src directory.
-directories=(
-    "cert"
-    "misc"
-)
-
-src_files=(
-    "ClangTidy.h"
-    "ClangTidyCheck.h"
-    "ClangTidyDiagnosticConsumer.h"
-    "ClangTidyModule.h"
-    "ClangTidyModuleRegistry.h"
-    "ClangTidyOptions.h"
-    "ClangTidyProfiling.h"
-    "ExpandModularHeadersPPCallbacks.h"
-    "FileExtensionsSet.h"
-    "GlobList.h"
-    "NoLintDirectiveHandler.h"
-)
-
-for directory in "${directories[@]}"; do
-    ln -f -s "../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/${directory}" "src/${directory}"
-done
-
-for file in "${src_files[@]}"; do
-    ln -f -s "../llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/${file}" "src/${file}"
-done
 
 # Copy the prepatched files to activate Automotive support
+cp tool/ClangTidyDiagnosticMapping.cpp llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.cpp
+cp tool/ClangTidyDiagnosticMapping.h llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyDiagnosticMapping.h
 cp prepatch/ClangTidyForceLinker.h llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
 cp prepatch/ClangTidy.cpp llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidy.cpp
 cp prepatch/ClangTidyOptions.cpp llvm-project-llvmorg-20.1.8/clang-tools-extra/clang-tidy/ClangTidyOptions.cpp
