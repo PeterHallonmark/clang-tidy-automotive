e4c988b8f3f0b422a2a4e14f40ce964ec31431c9
Peter Hallonmark <peter.hallonmark@gmail.com>
2026-03-29 22:35:53 +0200

    Added a dummy script until the real script actually works.

---
+++ scripts/check-copyright.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main() -> int:
+    print("TODO: Add some code to check for violations.")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
